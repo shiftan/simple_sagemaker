5300672b0fa1d885c6c03b2fe94c6204530ba906
Ariel Shiftan <shiftan@gmail.com>
2020-09-07 11:29:24 +0000

    Fix the build

--- tests/smoke/test_basics.py
+++ tests/smoke/test_basics.py
@@ -1,5 +1,6 @@
 import logging
 
+import boto3
 
 
 def test_project(caplog, tmp_path):
@@ -7,14 +8,17 @@ def test_project(caplog, tmp_path):
     logging.info("test_project")
 
     from simple_sagemaker.sm_project import SageMakerProject
+
     smProject = SageMakerProject(projectName="test")
     smProject = smProject
+
 
 def test_task(caplog, tmp_path):
     caplog.set_level(logging.INFO)
     logging.info("test_task")
 
     from simple_sagemaker.sm_task import SageMakerTask
+
     boto3Session = boto3.Session()
     imageUri = None
     smTask = SageMakerTask(boto3Session, "taskName", imageUri, prefix="tests/smoke")
