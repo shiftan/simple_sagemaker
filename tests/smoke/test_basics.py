d25575b316f88904ebc757e3bf6686030c0146e1
Ariel Shiftan <shiftan@gmail.com>
2020-09-07 11:27:30 +0000

    Update smoke tests

--- tests/smoke/test_basics.py
+++ tests/smoke/test_basics.py
@@ -1,10 +1,21 @@
 import logging
 
-from simple_sagemaker.sm_project import SageMakerProject
 
 
-def test_test(caplog, tmp_path):
+def test_project(caplog, tmp_path):
     caplog.set_level(logging.INFO)
-    logging.info("test_test")
+    logging.info("test_project")
+
+    from simple_sagemaker.sm_project import SageMakerProject
     smProject = SageMakerProject(projectName="test")
     smProject = smProject
+
+def test_task(caplog, tmp_path):
+    caplog.set_level(logging.INFO)
+    logging.info("test_task")
+
+    from simple_sagemaker.sm_task import SageMakerTask
+    boto3Session = boto3.Session()
+    imageUri = None
+    smTask = SageMakerTask(boto3Session, "taskName", imageUri, prefix="tests/smoke")
+    smTask = smTask
