b0d46b6a1040aaf43e3a2917044cd1fa6ef691d9
Ariel Shiftan <shiftan@gmail.com>
2020-09-12 23:12:04 +0000

    Create second readme example

--- examples/readme_examples/run.sh
+++ examples/readme_examples/run.sh
@@ -2,4 +2,8 @@
 
 # Params: [output] [prefix] [suffix] [additional ssm params...]
 BASEDIR=$(dirname "$0")
-ssm -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker.py -o $1 ${@:4}
+
+# Example 1
+ssm -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker1.py -o $1 ${@:4}
+# Example 2
+ssm -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker2.py -o $1 ${@:4}
