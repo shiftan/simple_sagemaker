c02cabbc40c60c73e7394aa4daf29b02216909b0
Ariel Shiftan <shiftan@gmail.com>
2020-09-13 12:13:31 +0000

    Workflow - upload examples output on failure as well

--- examples/readme_examples/run.sh
+++ examples/readme_examples/run.sh
@@ -11,12 +11,17 @@ ssm -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker1.py -o $
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task2 -e $BASEDIR/worker2.py --msg "Hello, world!" -o $1/example2 ${@:4} &
 
 # Example 3 - outputs
-( ssm -p ${2}simple-sagemaker-example-cli${3} -t task3 -e $BASEDIR/worker3.py -o $1/example3 ${@:4} &&
-# Example 5 - chaining data, using task3's output
-ssm -p ${2}simple-sagemaker-example-cli${3} -t task5 -e $BASEDIR/worker4.py --iit bucket task3 model -o $1/example5 ${@:4}) &
+ssm -p ${2}simple-sagemaker-example-cli${3} -t task3 -e $BASEDIR/worker3.py -o $1/example3 ${@:4} &
 
 # Example 4 - Inputs, using a local file + s3 bucket
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task4 -e $BASEDIR/worker4.py -i $BASEDIR/data --iis bucket s3://awsglue-datasets/examples/us-legislators/all/persons.json -o $1/example4 ${@:4} &
 
+wait # wait for all processes
+
+# running task3 again
+ssm -p ${2}simple-sagemaker-example-cli${3} -t task3 -e $BASEDIR/worker3.py -o $1/example3_2 ${@:4} > $1/example3_2_stdout &
+
+# Example 5 - chaining data, using task3's output
+ssm -p ${2}simple-sagemaker-example-cli${3} -t task5 -e $BASEDIR/worker4.py --iit bucket task3 model -o $1/example5 ${@:4} &
 
 wait # wait for all processes
