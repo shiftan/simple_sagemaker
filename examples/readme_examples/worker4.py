139f4a493570d5eef3d346f604e612bd8f2fa10e
Ariel Shiftan <shiftan@gmail.com>
2020-09-13 13:14:50 +0000

    Update tests and expected output

--- examples/readme_examples/worker4.py
+++ examples/readme_examples/worker4.py
@@ -1,16 +1,25 @@
+import logging
 import subprocess
+import sys
 
 from task_toolkit import algo_lib
 
+logger = logging.getLogger(__name__)
+
 
 def listDir(path):
-    print(
+    logger.info(f"*** START listing files in {path}")
+    logger.info(
         subprocess.run(
             ["ls", "-la", "-R", path], stdout=subprocess.PIPE, universal_newlines=True
         ).stdout
     )
+    logger.info(f"*** END file listing {path}")
 
 
-args = algo_lib.parseArgs()
-listDir(args.input_data)
-listDir(args.input_bucket)
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout)
+    algo_lib.setDebugLevel()
+    args = algo_lib.parseArgs()
+    listDir(args.input_data)
+    listDir(args.input_bucket)
