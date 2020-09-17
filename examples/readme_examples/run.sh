31ff6cc1722a37585812ec3b08db4ea5b729ec44
Ariel Shiftan <shiftan@gmail.com>
2020-09-17 08:13:07 +0000

    Update readme examples expected output

--- examples/readme_examples/run.sh
+++ examples/readme_examples/run.sh
@@ -19,7 +19,7 @@ wait # wait for all processes
 # Example 6_2 - a complete example part 2 - uses outputs from part 1
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-2 -s $BASEDIR/example6/code -e worker6.py \
     -d $BASEDIR/example6/external_dependency --iit task_6_1_model task6-1 model --iit task_6_1_state task6-1 state ShardedByS3Key \
-    --ic 2 --task_type 2 -o $1/example6 ${@:4} &
+    --ic 2 --task_type 2 -o $1/example6_2 ${@:4} &
 
 
 # running task6_1 again with --ks (keep state) to demonstrate tgat existing output is used, without running the task again
