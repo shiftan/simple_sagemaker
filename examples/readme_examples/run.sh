7f5a239104d0d62ad9d1b8ca575d071bfc812a73
Ariel Shiftan <shiftan@gmail.com>
2020-09-17 01:00:41 +0000

    Fix the complete readme example

--- examples/readme_examples/run.sh
+++ examples/readme_examples/run.sh
@@ -7,7 +7,7 @@ BASEDIR=$(dirname "$0")
 # Example 1 - hello world
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker1.py -o $1/example1 --it ml.p3.2xlarge ${@:4} &
 
-# Example 6_1 - a complete example
+# Example 6_1 - a complete example part 1
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code -e $BASEDIR/example6/code/worker6.py \
     -i $BASEDIR/example6/data ShardedByS3Key --iis persons s3://awsglue-datasets/examples/us-legislators/all/persons.json \
     --df $BASEDIR/example6/Dockerfile --repo_name "task6_repo", --aws_repo "task6_repo" \
@@ -15,10 +15,10 @@ ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code
 
 wait # wait for all processes
 
-# Example 6_2 - a complete example
+# Example 6_2 - a complete example part 1
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-2 -s $BASEDIR/example6/code -e $BASEDIR/example6/code/worker6.py \
     -d $BASEDIR/example6/external_dependency --iit task_6_1_model task6-1 model --iit task_6_1_state task6-1 state ShardedByS3Key \
-     --task_type 2 -o $1/example5 ${@:4} &
+    --ic 2 --task_type 2 -o $1/example5 ${@:4} &
 
 
 # running task6_1 again
