c2fa8608862068ddfdeffb84263b99f8bb155aa5
shiftan <shiftan@gmail.com>
2020-09-21 19:03:45 +0300

    Update run.sh

--- examples/readme_examples/run.sh
+++ examples/readme_examples/run.sh
@@ -18,6 +18,7 @@ ssm -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker1.py -o $
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code -e worker6.py \
     -i $BASEDIR/example6/data ShardedByS3Key --iis persons s3://awsglue-datasets/examples/us-legislators/all/persons.json \
     --df $BASEDIR/example6 --repo_name "task6_repo" --aws_repo "task6_repo" --no_spot \
+    --download_state --download_model --download_output \
     --ic 2 --task_type 1 -o $1/example6_1 ${@:4}
 
 # Example 6_2 - a complete example part 2.
@@ -29,6 +30,7 @@ ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-2 -s $BASEDIR/example6/code -e worker6.py \
     -d $BASEDIR/example6/external_dependency --iit task_6_1_model task6-1 model --iit task_6_1_state task6-1 state ShardedByS3Key \
     -f tensorflow -m --md "Score" "Score=(.*?);" --tag "MyTag" "MyValue" --no_spot \
+    --download_state --download_model --download_output \
     --ic 2 --task_type 2 -o $1/example6_2 ${@:4} &
 
 # Running task6_1 again
@@ -36,6 +38,7 @@ ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-2 -s $BASEDIR/example6/code
 ssm -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code -e worker6.py \
     -i $BASEDIR/example6/data ShardedByS3Key --iis persons s3://awsglue-datasets/examples/us-legislators/all/persons.json \
     --df $BASEDIR/example6 --repo_name "task6_repo" --aws_repo "task6_repo" \
+    --download_state --download_model --download_output \
     --ic 2 --task_type 1 -o $1/example6_1 ${@:4} > $1/example6_1_2_stdout --ks &
 
 
