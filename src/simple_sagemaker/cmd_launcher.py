53642c50a307b64d8dec4a1ef1485e046934899d
Ariel Shiftan <shiftan@gmail.com>
2020-09-23 19:43:51 +0000

    Remove unneeded code

--- src/simple_sagemaker/cmd_launcher.py
+++ src/simple_sagemaker/cmd_launcher.py
@@ -8,24 +8,11 @@ from worker_toolkit import worker_lib
 logger = logging.getLogger(__name__)
 
 
-def listDir(path, ignore_patterns=[]):
-    logger.info(f"*** START listing files in {path}")
-    for file in sorted(Path(path).rglob("*")):
-        if (not ignore_patterns) or all(
-            [pattern not in str(file) for pattern in ignore_patterns]
-        ):
-            logger.info(f"[{['Dir ', 'File'][file.is_dir()]}] {file}")
-    logger.info(f"*** END file listing {path}")
-
-
 def worker():
     logging.basicConfig(stream=sys.stdout)
 
     # parse the arguments
     worker_config = worker_lib.WorkerConfig()
-    listDir("/opt/ml")
-    logger.info(f"Current directory: {Path('.').resolve()}")
-    listDir(".")
 
     cmd_line = worker_config.hps["SSM_CMD_LINE"]
     logger.info(f"Launching: {cmd_line}")
