0884d0a4059ad59ddf41b49f31bd13b4af598e89
Ariel Shiftan <shiftan@gmail.com>
2020-09-24 11:53:18 +0000

    Minor naming examples update

--- examples/readme_examples/run.sh
+++ examples/readme_examples/run.sh
@@ -6,7 +6,7 @@ BASEDIR=$(dirname "$0")
 echo "Running with", $@
 
 # Example 1 - hello world
-ssm run -p ${2}simple-sagemaker-example-cli${3} -t task1 -e $BASEDIR/worker1.py -o $1/example1 --it ml.p3.2xlarge --ic 2 ${@:4} --max_run_mins 15 &
+ssm run -p ${2}simple-sagemaker-example-cli${3} -t cli-task1 -e $BASEDIR/worker1.py -o $1/example1 --it ml.p3.2xlarge --ic 2 ${@:4} --max_run_mins 15 &
 
 # Shell example
 ssm shell -p ${2}simple-sagemaker-example-cli${3} -t shell-task --cmd_line "cat /proc/cpuinfo && nvidia-smi" --no_spot -o $1/example_cmd --it ml.p3.2xlarge ${@:4} --max_run_mins 15 &
@@ -20,7 +20,7 @@ wait # wait for all processes
 #   - Hyperparameter task_type
 #   - 2 instance (--ic)
 #   - Use an on-demand instance (--no_spot)
-ssm run -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code -e worker6.py \
+ssm run -p ${2}simple-sagemaker-example-cli${3} -t cli-task6-1 -s $BASEDIR/example6/code -e worker6.py \
     -i $BASEDIR/example6/data ShardedByS3Key --iis persons s3://awsglue-datasets/examples/us-legislators/all/persons.json \
     --df $BASEDIR/example6 --repo_name "task6_repo" --aws_repo "task6_repo" --no_spot \
     --download_state --download_model --download_output --max_run_mins 15 \
@@ -32,15 +32,15 @@ ssm run -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/
 #   - Uses the tensorflow framework as pre-built image (-f)
 #   - Tags the jobs (--tag)
 #   - Defines sagemaker metrics (-m, --md)
-ssm run -p ${2}simple-sagemaker-example-cli${3} -t task6-2 -s $BASEDIR/example6/code -e worker6.py \
-    -d $BASEDIR/example6/external_dependency --iit task_6_1_model task6-1 model --iit task_6_1_state task6-1 state ShardedByS3Key \
+ssm run -p ${2}simple-sagemaker-example-cli${3} -t cli-task6-2 -s $BASEDIR/example6/code -e worker6.py \
+    -d $BASEDIR/example6/external_dependency --iit task_6_1_model task6-1 model --iit task_6_1_state cli-task6-1 state ShardedByS3Key \
     -f tensorflow -m --md "Score" "Score=(.*?);" --tag "MyTag" "MyValue" --no_spot \
     --download_state --download_model --download_output --max_run_mins 15 \
     --ic 2 --task_type 2 -o $1/example6_2 ${@:4} &
 
 # Running task6_1 again
 #   --ks (keep state) is used to keep the current state and demonstrate that existing output is used, without running the task again
-ssm run -p ${2}simple-sagemaker-example-cli${3} -t task6-1 -s $BASEDIR/example6/code -e worker6.py \
+ssm run -p ${2}simple-sagemaker-example-cli${3} -t cli-task6-1 -s $BASEDIR/example6/code -e worker6.py \
     -i $BASEDIR/example6/data ShardedByS3Key --iis persons s3://awsglue-datasets/examples/us-legislators/all/persons.json \
     --df $BASEDIR/example6 --repo_name "task6_repo" --aws_repo "task6_repo" \
     --download_state --download_model --download_output --max_run_mins 15 \
