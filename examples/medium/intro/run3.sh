beb526abfc11995fb3fcc00cfbbbec3916947d19
Ariel Shiftan <shiftan@gmail.com>
2020-09-29 21:30:56 +0000

    Fix medium example run script

--- examples/medium/intro/run3.sh
+++ examples/medium/intro/run3.sh
@@ -7,12 +7,12 @@ ssm run -p ssm-ex -t ex3-1 -s ./code -e ssm_ex3_worker.py \
     --iis persons s3://awsglue-datasets/examples/us-legislators/all/persons.json \
     --df "RUN pip3 install pandas==0.25.3 scikit-learn==0.21.3" \
     --repo_name "ex3_repo" --aws_repo_name "ex3_repo" --no_spot \
-    --ic 2 --task_type 1 -o $1/ex3_1 --cs
+    --ic 2 --task_type 1 -o ./out3/ex3_1 --cs
 
 ssm run -p ssm-ex -t ex3-2 -s ./code -e ssm_ex3_worker.py \
     -d ./external_dependency --iit ex3_1_model ex3-1 model \
     --iit ex3_1_state ex3-1 state ShardedByS3Key \
     -f tensorflow -m --md "Score" "Score=(.*?);" --tag "MyTag" "MyValue" \
-    --ic 2 --task_type 2 -o $1/ex3_2 --cs
+    --ic 2 --task_type 2 -o ./out3/ex3_2 --cs
 
 popd
