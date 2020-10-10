df0607f3f2f157048e0a07fed8ba9800502b3a63
Ariel Shiftan <shiftan@gmail.com>
2020-10-10 18:48:31 +0000

    Medium intro - update running scripts

--- examples/medium/intro/run1.sh
+++ examples/medium/intro/run1.sh
@@ -3,9 +3,8 @@ pushd .
 cd $BASEDIR
 
 # Clean the current state to make sure the code runs again
-#   Note:   1. This is done just as part of the example
-#           2. It can done by appending "--cs" to the "ssm shell" command below
-ssm data -p ssm-ex -t ex1 --cs
+#   Note:   1. It is done just for demonstration, by appending "--force_running" to the "ssm shell" command below
+ssm data -p ssm-ex -t ex1 --force_running
 # Run the task
 ssm shell -p ssm-ex -t ex1 -o ./out1 --it ml.p3.2xlarge --cmd_line "cat /proc/cpuinfo && nvidia-smi"
 
