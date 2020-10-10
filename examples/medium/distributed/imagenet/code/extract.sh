863c8e4d25b33d33e4f13474d6e89815547f71bb
Ariel Shiftan <shiftan@gmail.com>
2020-10-10 18:41:36 +0000

    Imagenet example - parallel extraction of imagenet train files

--- examples/medium/distributed/imagenet/code/extract.sh
+++ examples/medium/distributed/imagenet/code/extract.sh
@@ -6,11 +6,18 @@ set -e # stop and fail if anything stops
 echo "Extracting all..."
 cd $1
 
-for filename in train/*.tar; do
+extract_and_delete() {
+    filename=$1
     OUTDIR=${filename%.tar}
     tar -xf $filename --xform="s|^|$OUTDIR/|S"
-    rm $filename
+    rm $filename    
+}
+
+for filename in train/*.tar; do
+    extract_and_delete $filename &
 done
+
+wait
 
 cd val
 # https://github.com/facebookarchive/fb.resnet.torch/blob/master/INSTALL.md
