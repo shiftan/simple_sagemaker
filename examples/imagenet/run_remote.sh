90f6235ba5980a124ab9d5a865a6531dbe23dc2c
Ariel Shiftan <shiftan@gmail.com>
2020-10-12 13:24:13 +0000

    Minor imagenet fix

--- examples/imagenet/run_remote.sh
+++ examples/imagenet/run_remote.sh
@@ -27,7 +27,7 @@ run_training () { # args: task_name, instance_type, additional_command_params, [
         --md "loss" "Epoch:.*Loss\s+([e\-+0-9\\.]*) \(" --md "acc1" "Epoch:.*Acc@1\s+([e\-+0-9\\.]*) \(" --md "acc5" "Epoch:.*Acc@5\s+([e\-+0-9\\.]*) \(" \
         --md "time" "Epoch:.*Time\s+([e\-+0-9\\.]*) \(" --md "data_time" "Epoch:.*Data\s+([e\-+0-9\\.]*) \(" \
         --md "test_loss" "Test:.*Loss\s+([e\-+0-9\\.]*) \(" --md "test_acc1" "Test:.*Acc@1\s+([e\-+0-9\\.]*) \(" --md "test_acc5" "Test:.*Acc@5\s+([e\-+0-9\\.]*) \(" \
-        --download_model --download_output -- download_state \
+        --download_model --download_output --download_state \
         --it $2 $ADDITIONAL_ARGS \
         --cmd_line  "./extract.sh \$SM_CHANNEL_TRAIN/.. && \ 
                     CODE_DIR=\`pwd\` && cd \$SSM_INSTANCE_STATE && START=\$SECONDS && \
